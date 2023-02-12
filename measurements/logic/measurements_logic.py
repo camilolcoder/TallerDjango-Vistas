069452d191420e6b71f9d46c61caeeda5d2389d9
Camilo Colmenares <j.colmenares@uniandes.edu.co>
2023-02-11 19:14:36 -0500

    delete added

--- measurements/logic/measurements_logic.py
+++ measurements/logic/measurements_logic.py
@@ -1,24 +1,30 @@
 from ..models import Measurement
 import variables.logic.variables_logic as vg
 
-def get_variables():
+def get_measurements():
     measurements = Measurement.objects.all()
     return measurements
 
-def get_variable(mea_pk):
+def get_measurement(mea_pk):
     measurement = Measurement.objects.get(pk=mea_pk)
     return measurement
 
-def update_variable(mea_pk, new_mea):
-    measurement = get_variable(mea_pk)
+def update_measurement(mea_pk, new_mea):
+    measurement = get_measurement(mea_pk)
     measurement.unit = new_mea["unit"]
     measurement.value = new_mea["value"]
+    measurement.place = new_mea["palce"]
     measurement.save()
     return measurement
 
-def create_variable(mea):
+def create_measurement(mea):
     measurement = Measurement(variable=vg.get_variable(mea["variable"]),
                               unit= mea["unit"],
-                              value= mea["value"])
+                              value= mea["value"],
+                              place=mea["palce"])
     measurement.save()
     return measurement
+
+def delete_measurement(mea_pk):
+    measurement = Measurement.objects.get(mea_pk)
+    measurement.delete()
